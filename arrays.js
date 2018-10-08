119cd221f125d63d40b2797fde6905255bfb7219
Samuel Ellis <pleasecastsamellis@gmail.com>
2018-10-08 00:47:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,7 +9,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
  return array;
 }
 function addElementToEndOfArray(array, element) {
-  return [...array, 'element'];
+  return [...array, element];
 }
 function destructivelyAddElementToEndOfArray(array, element) {
   array = array.push(element);
